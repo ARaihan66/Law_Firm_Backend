8d2a05636af57968e3725650433dd267add0138f
Abu Raihan <aburaihancse066@gmail.com>
2024-03-22 23:49:59 +0600

    Check vercel link

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 require("dotenv").config();
 const app = require("./app");
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 //Database Connection
-mongoose.connect(process.env.DB_URL, {
+mongoose
+  .connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then((data) => {
-    console.log("Database is connected successfully!!!")
-}).catch((error) => {
-    console.log("Database connection is failed!!!")
-})
+  })
+  .then((data) => {
+    console.log("Database is connected successfully!!!");
+  })
+  .catch((error) => {
+    console.log("Database connection is failed!!!");
+  });
+
+app.get("/", (req, res) => {
+  return "Hello world";
+});
 
 // Create Sever
 app.listen(process.env.PORT, () => {
-    console.log(`Server is successfully running on port ${process.env.PORT}`)
-})
+  console.log(`Server is successfully running on port ${process.env.PORT}`);
+});
