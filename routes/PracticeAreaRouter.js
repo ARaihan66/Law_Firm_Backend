301eee16c87041942dee129587715f9957066abd
Abu Raihan <aburaihancse066@gmail.com>
2024-04-01 22:25:52 +0600

    add some changes

--- routes/PracticeAreaRouter.js
+++ routes/PracticeAreaRouter.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const {addPracticeArea,getPracticeAreaData,updatePracticeAreaData,deletePracticeAreaData} = require('../controllers/PracticeAreaController')
-const verifyAdmin = require("../utils/VerifyAdmin")
+const {
+  addPracticeArea,
+  getPracticeAreaData,
+  updatePracticeAreaData,
+  deletePracticeAreaData,
+} = require("../controllers/PracticeAreaController");
+const verifyAdmin = require("../utils/VerifyAdmin");
 
-
-router.route("/add")
-      .post(verifyAdmin,addPracticeArea)
-router.route("/get")
-      .get(verifyAdmin,getPracticeAreaData)
-router.route("/client/get")
-      .get(getPracticeAreaData)
-router.route("/update/:id")
-      .put(verifyAdmin,updatePracticeAreaData)
-router.route("/delete/:id")
-      .delete(verifyAdmin,deletePracticeAreaData)
-
+router.route("/add").post(verifyAdmin, addPracticeArea);
+router
+  .route("/get")
+  //.get(verifyAdmin,getPracticeAreaData)
+  .get(getPracticeAreaData);
+router.route("/client/get").get(getPracticeAreaData);
+router.route("/update/:id").put(verifyAdmin, updatePracticeAreaData);
+router.route("/delete/:id").delete(verifyAdmin, deletePracticeAreaData);
 
 module.exports = router;
