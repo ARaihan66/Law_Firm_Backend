f91e5c566a1e2a16b57836be01165d45ae2c0fbe
Abu Raihan <aburaihancse066@gmail.com>
2024-07-27 00:23:38 +0600

    Again Modify Admin

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static("upload"));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "http://admin.rasmul.com"],
+  origin: ["http://localhost:3000", "http://localhost:3000"],
   methods: "GET,POST,PUT,DELETE",
   credentials: true,
 };
